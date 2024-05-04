3d03ca5cce4411da0fa34c470795f6f96283dc93
Giacomo Leidi <goodoldpaul@autistici.org>
2024-05-04 22:00:00 +0200

    helpers: Add function to get generic resources, tags and taggings.

--- pyactionnetwork/helpers.py
+++ pyactionnetwork/helpers.py
@@ -3,17 +3,41 @@
 
 import requests
 
-from .models import Donation
+from .models import Donation, Tag, Tagging
 
 
-def get_all_donations(api=None, donations=None, url="https://actionnetwork.org/api/v2/donations"):
+def get_all_resource(resource, cls, api, url, resources=[]):
+    """Get a list of all resources for an organization.
+    Args:
+        resource (str):
+            resource name (e.g. 'links', 'people', etc.)
+        cls (class):
+            A class from the models module representing the resource (Tag, Person, etc.)
+        api (pyactionnetwork.ActionNetworkApi):
+            Authorized ActionNetwork API instance.
+        url (str):
+            URL of the endpoint to use.
+        resources (list):
+            List of cls instances.
+
+    Returns:
+        (list) List of Donations processed by AN.
+    """
+
+    data = api.request_json("GET", url)
+    resources += [cls(data=d) for d in data['_embedded'][f"osdi:{resource}"]]
+
+    if data.get('_links', {}).get('next', None):
+        next_url = data.get('_links').get('next').get('href')
+        return get_all_resource(resource=resource, cls=cls, api=api, url=next_url, resources=resources)
+    return resources
+
+def get_all_donations(api=None, url=None):
     """Get a list of all donations for an organization.
 
     Args:
         api (pyactionnetwork.ActionNetworkApi):
             Authorized ActionNetwork API instance.
-        donations (list):
-            List of Donation instances.
         url (str):
             URL of the donations endpoint to use. Defaults to all
             donations made to a group.
@@ -21,13 +45,43 @@ def get_all_donations(api=None, donations=None, url="https://actionnetwork.org/a
     Returns:
         (list) List of Donations processed by AN.
     """
-    if not donations:
-        donations = []
+    if not url:
+        url = api.resource_to_url("donations")
 
-    data = requests.get(url=url, headers=api.headers)
-    donations += [Donation(data=d) for d in data.json()['_embedded']['osdi:donations']]
+    return get_all_resource("donations", Donation, api=api, url=url)
 
-    if data.json().get('_links', {}).get('next', None):
-        next_url = data.json().get('_links').get('next').get('href')
-        return get_all_donations(api=api, donations=donations, url=next_url)
-    return donations
+def get_all_tags(api=None, url=None):
+    """Get a list of all tags for an organization.
+
+    Args:
+        api (pyactionnetwork.ActionNetworkApi):
+            Authorized ActionNetwork API instance.
+        url (str):
+            URL of the tags endpoint to use. Defaults to all
+            tags made to a group.
+
+    Returns:
+        (list) List of Tag on AN.
+    """
+    if not url:
+        url = api.resource_to_url("tags")
+
+    return get_all_resource("tags", Tag, api=api, url=url)
+
+def get_all_taggings(api=None, url=None):
+    """Get a list of all taggings for an organization.
+
+    Args:
+        api (pyactionnetwork.ActionNetworkApi):
+            Authorized ActionNetwork API instance.
+        url (str):
+            URL of the taggings endpoint to use. Defaults to all
+            taggings made to a group.
+
+    Returns:
+        (list) List of Tagging on AN.
+    """
+    if not url:
+        url = api.resource_to_url("taggings")
+
+    return get_all_resource("taggings", Tagging, api=api, url=url)
