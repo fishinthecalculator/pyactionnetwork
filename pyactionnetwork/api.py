77713276d6aa9a3ac4b0b2b4a6cb52d42c5a887c
Giacomo Leidi <goodoldpaul@autistici.org>
2024-05-04 21:41:47 +0200

    api: Add general method to make requests.

--- pyactionnetwork/api.py
+++ pyactionnetwork/api.py
@@ -15,10 +15,24 @@ class ActionNetworkApi:
         self.base_url = self.config.get('links', {}).get('self', 'https://actionnetwork.org/api/v2/')
         print(self.config['motd'])
 
+    def request(self, verb, url, json=None):
+        kwargs = {
+            "headers": self.headers,
+            "url": url
+        }
+        if json:
+            kwargs.update({"json": json})
+        return requests.request(
+            verb,
+            **kwargs
+        )
+
+    def request_json(self, verb, url, json=None):
+        return self.request(verb, url, json=json).json()
+
     def refresh_config(self):
         """Get a new version of the base_url config."""
-        self.config = requests.get(url="https://actionnetwork.org/api/v2/",
-                                   headers=self.headers).json()
+        self.config = self.request_json("GET", url="https://actionnetwork.org/api/v2/")
 
     def resource_to_url(self, resource):
         """Convert a named endpoint into a URL.
@@ -46,7 +60,7 @@ class ActionNetworkApi:
             (dict) API response from endpoint or `None` if not found/valid.
         """
         url = self.resource_to_url(resource)
-        return requests.get(url, headers=self.headers).json()
+        return self.request_json("GET", url=url)
 
     def get_person(self, person_id=None, search_by='email', search_string=None):
         """Search for a user.
@@ -68,8 +82,7 @@ class ActionNetworkApi:
                 search_by,
                 quote(search_string))
 
-        resp = requests.get(url, headers=self.headers)
-        return resp.json()
+        return self.request_json("GET", url=url)
 
     def create_person(self,
                       email=None,
@@ -132,8 +145,7 @@ class ActionNetworkApi:
             'add_tags': list(tags)
         }
 
-        resp = requests.post(url, json=payload, headers=self.headers)
-        return resp.json()
+        return self.request_json("POST", url=url, json=payload)
 
     def update_person(self,
                       person_id=None,
@@ -192,8 +204,7 @@ class ActionNetworkApi:
             'custom_fields': custom_fields,
         }
 
-        resp = requests.put(url, json=payload, headers=self.headers)
-        return resp.json()
+        return self.request_json("PUT", url=url, json=payload)
 
     def search(self, resource, operator, term):
         """Search for a given `term` within a `resource`.
